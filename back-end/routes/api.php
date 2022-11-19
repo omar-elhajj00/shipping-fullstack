c1d05508df394899ea7723e1eeaa373a92d125e5
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-11-19 14:22:35 +0200

    implement the register function

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,3 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('register', [UserController::class, 'register']);
-Route::post('login', [UserController::class, 'login']);
