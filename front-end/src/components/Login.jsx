d03a47d62824d36e2861cfb5ad1e1d8f34b56faf
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-11-19 07:03:56 +0200

    add the avatar to login page

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import {Grid} from '@mui/material';
+import {Avatar, Grid, Paper} from '@mui/material';
 
 const Login = () => {
+    const paperStyle={padding:20, height: '70vh', width:200 ,margin: '20px auto'};
+
     return(
         <Grid>
-            
+            <Paper elevation={1} style={paperStyle}>
+                <Avatar></Avatar>
+                login</Paper>
         </Grid>
     )
 }
