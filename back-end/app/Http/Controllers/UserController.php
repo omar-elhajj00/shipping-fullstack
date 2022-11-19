1510778d2ae02ad288a90039bbcf71508e6a901e
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-11-19 13:28:20 +0200

    implement the register function into usercontroller

--- back-end/app/Http/Controllers/UserController.php
+++ back-end/app/Http/Controllers/UserController.php
@@ -9,6 +9,15 @@ class UserController extends Controller
     //
     function register(Request $req){
         $user = new User;
-        
+        $user-> name= $req->input('name');
+        $user-> email= $req->input('email');
+        $user-> password= Hash::make($req->input('password'));
+        $user->save();
+        return $user;
+
+    }
+
+    function login(){
+        return 'login';
     }
 }
