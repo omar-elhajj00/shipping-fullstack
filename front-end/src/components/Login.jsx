f0179981f8ad6dd21e2a425155604aa5091cc814
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-11-19 07:24:43 +0200

    add the username field into login page

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import {Avatar, Grid, Paper} from '@mui/material';
+import {Avatar, Grid, Paper, TextField} from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
 
 const Login = () => {
     const paperStyle={padding:20, height: '70vh', width:200 ,margin: '20px auto'};
-
+    const avatartStyle={backgroundColor:'green'};
     return(
         <Grid>
             <Paper elevation={1} style={paperStyle}>
-                <Avatar></Avatar>
-                login</Paper>
+                <Grid align="center">
+                    <Avatar style={avatartStyle}><LockOutlinedIcon /></Avatar>
+                    <h2>Login</h2> 
+                </Grid>
+                <TextField id="standard-basic" label="Username" placeholder="HAHA" />
+                
+            </Paper>
         </Grid>
     )
 }
