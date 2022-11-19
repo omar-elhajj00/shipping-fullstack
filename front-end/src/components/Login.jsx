88d3d501c0e07629f622ed63481f8047e10ab6c5
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-11-19 06:55:51 +0200

    add the login component

---
+++ front-end/src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Grid} from '@mui/material';
+
+const Login = () => {
+    return(
+        <Grid>
+            
+        </Grid>
+    )
+}
+
+export default Login
