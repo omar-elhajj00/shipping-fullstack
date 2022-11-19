54df05e9c1f22a947b2466156238322d9dccad76
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-11-19 08:22:11 +0200

    add the sign in button

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import {Avatar, Grid, Paper, TextField} from '@mui/material';
+import {Avatar, Button, FormGroup, Grid, Paper, TextField} from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import FormControlLabel from "@mui/material/FormControlLabel/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox/Checkbox";
 
 
 const Login = () => {
     const paperStyle={padding:20, height: '70vh', width:200 ,margin: '20px auto'};
     const avatartStyle={backgroundColor:'green'};
-    const inputStyle={marginBottom:1};
+    const inputStyle={margin:10};
     return(
         <Grid>
             <Paper elevation={1} style={paperStyle}>
@@ -17,7 +19,10 @@ const Login = () => {
                 
                 <TextField label="Username" placeholder="Enter you username" variant="standard" fullwidth required style={inputStyle}/>
                 <TextField label="Password" placeholder="Enter you username" type="password" variant="standard" fullwidth required style={inputStyle}/>
-            
+                <FormGroup>
+                    <FormControlLabel control={<Checkbox />} label="Remember me" style={inputStyle} />
+                </FormGroup>
+                <Button type="submit" color="primary" variant="contained" fullWidth>Sign in</Button>
 
                 
             </Paper>
