8220a8557effb978d9c7175d89784c0162ac7abb
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-11-21 17:49:54 +0200

    adjust the input field into create shipment component

--- front-end/src/components/CreateShipment.jsx
+++ front-end/src/components/CreateShipment.jsx
@@ -3,11 +3,11 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-import { TextField } from '@mui/material';
+import { Grid, TextField, Tooltip } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 
 
-const CreateShipment = ({open},{setOpen}) => {
+const CreateShipment = ({open,setOpen}) => {
     const style = {
         position: 'absolute',
         top: '50%',
@@ -21,7 +21,7 @@ const CreateShipment = ({open},{setOpen}) => {
       };
       
     //   const handleOpen = () => setOpen(true);
-      const handleClose = () => setOpen(false);
+    const handleClose = () => setOpen(false);
   return (
     <Modal
         open={open}
@@ -30,14 +30,30 @@ const CreateShipment = ({open},{setOpen}) => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style} >
-          <Typography id="modal-modal-title" variant="h5" component="h2" sx={{ mb: 2 }}>
-            Create Shipment
-          </Typography> 
-          <TextField id="standard-basic" label="paybill" variant="standard" placeholder='PayBill' sx={{ mb: 2 }} fullWidth/>
-          <TextField id="standard-basic" label="customer address" variant="standard" placeholder='customer address' sx={{ mb: 2 }} fullWidth/>
-          <TextField id="standard-basic" label="customer name" variant="standard" placeholder='customer name' sx={{ mb: 2 }} fullWidth/>
-          <TextField id="standard-basic" label="customer phone number" variant="standard" placeholder='customer phone number' sx={{ mb: 2 }} fullWidth/>
-          <Button variant="contained" size="large" >Submit</Button>
+            
+                <Grid direction={'row'} container justifyContent="space-between">
+                    <Grid item xs={6}>
+                        <Tooltip placement="right-start">
+                            <Typography id="modal-modal-title" variant='h6' component="h2" sx={{ mb:1 }} >
+                                Create Shipment
+                            </Typography>   
+                            
+                        </Tooltip>
+                    </Grid>
+                    <Grid item alignItems="flex-end">
+                        <Tooltip placement="right-end">
+                            <Button onClick={handleClose}><CloseIcon /></Button>
+                        </Tooltip>
+                    </Grid>
+                </Grid>
+                
+                
+            
+            <TextField id="standard-basic" label="paybill" variant="standard" placeholder='PayBill' sx={{ mb: 2 }} fullWidth/>
+            <TextField id="standard-basic" label="customer address" variant="standard" placeholder='customer address' sx={{ mb: 2 }} fullWidth/>
+            <TextField id="standard-basic" label="customer name" variant="standard" placeholder='customer name' sx={{ mb: 2 }} fullWidth/>
+            <TextField id="standard-basic" label="customer phone number" variant="standard" placeholder='customer phone number' sx={{ mb: 2 }} fullWidth/>
+            <Button variant="contained" size="large" >Submit</Button>
 
         </Box>
       </Modal>  
