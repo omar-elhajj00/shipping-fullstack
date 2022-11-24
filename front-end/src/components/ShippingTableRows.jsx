f397398ee1c2b189c2a4233a1e148e38fa853642
omar-elhajj00 <omar.elhajj.00@gmail.com>
2022-11-24 17:57:45 +0200

    clean and comment shipping tbale row component code

--- front-end/src/components/ShippingTableRows.jsx
+++ front-end/src/components/ShippingTableRows.jsx
@@ -5,43 +5,42 @@ import TableRow from '@mui/material/TableRow';
 import { Button } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
-import { deleteShipmentApi, getAllShipmentsRequest } from '../Apis/Apis';
-import axios from 'axios';
-
-import UpdateShipment from './UpdateShipment';
+import { deleteShipmentApi, getAllShipmentsRequest, UpdateShipment } from '../Apis/Apis';
 
 const ShippingTableRows = () => {
+    //database data 
     const [data, setData] = useState([]);
+    //update modal
     const [openUpdateShipment, setOpenUpdateShipment] = React.useState(false);
-    const handleOpenUpdateShipment = (item) => setOpenUpdateShipment(true);
-
+    const handleOpenUpdateShipment = () => setOpenUpdateShipment(true);
+    //when the home page load
     useEffect(() => {
         getAllShipmentsRequest({data,setData});    
     },[]);
-    console.warn("result", data); 
 
   return (
     <TableBody>
-            {
-                Array.from(data).map((item) => (
-                    <TableRow
-                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    >
-                        <TableCell component="th" scope="row">
-                            {item.paybill}  
-                        </TableCell>
-                        <TableCell align="right">{item.customer_name}</TableCell>
-                        <TableCell align="right">{item.customer_phone}</TableCell>
-                        <TableCell align="right">{item.customer_address}</TableCell>
-                        <TableCell align="right"> 
-                            <Button onClick={() => deleteShipmentApi(item.paybill)}><DeleteIcon /></Button>
-                            <Button onClick={handleOpenUpdateShipment}><EditIcon /></Button>
-                            <UpdateShipment open={openUpdateShipment} setOpen={setOpenUpdateShipment} shipmentInfo={item} />
-                        </TableCell>
-                    </TableRow>
-                    )
+        {
+            //each object inside the reponse json object
+            Array.from(data).map((item) => (
+                <TableRow
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                    <TableCell component="th" scope="row">
+                        {item.paybill}  
+                    </TableCell>
+                    <TableCell align="right">{item.customer_name}</TableCell>
+                    <TableCell align="right">{item.customer_phone}</TableCell>
+                    <TableCell align="right">{item.customer_address}</TableCell>
+                    <TableCell align="right"> 
+                    <Button onClick={() => deleteShipmentApi(item.paybill)}><DeleteIcon /></Button>
+                    <Button onClick={handleOpenUpdateShipment}><EditIcon /></Button>
+                    <UpdateShipment open={openUpdateShipment} setOpen={setOpenUpdateShipment} shipmentInfo={item} />
+                    </TableCell>
+                </TableRow>
                 )
-            }
+            )
+        }
     </TableBody>
   )
 }
